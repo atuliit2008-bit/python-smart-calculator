642f294b06eab292e4c062fe1f7cdf57ce6c1c12
Atul Mishra <atuliit2008@gmail.com>
2026-07-01 21:24:56 +0530

    modified the code in main.py make it more interactive.

--- main.py
+++ main.py
@@ -1,13 +1,67 @@
-# Import all calculator functions
+# """
+# main.py
+# Main program for Smart Calculator
+# """
+
 from calculator import *
 
-print("===== SMART CALCULATOR TEST =====")
 
-print("Addition:", add(10, 5))
-print("Subtraction:", subtract(10, 5))
-print("Multiplication:", multiply(10, 5))
-print("Division:", divide(10, 5))
-print("Power:", power(2, 3))
-print("Square Root:", square_root(25))
-print("Modulus:", modulus(10, 3))
-print("Floor Division:", floor_division(10, 3))
+def show_menu():
+    # """Display calculator menu"""
+    print("\n========== SMART CALCULATOR ==========")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Power")
+    print("6. Square Root")
+    print("7. Modulus")
+    print("8. Floor Division")
+    print("9. Exit")
+    print("======================================")
+
+
+while True:
+
+    show_menu()
+
+    choice = input("Enter your choice (1-9): ")
+
+    if choice == "9":
+        print("\nThank you for using Smart Calculator.")
+        break
+
+    if choice == "6":
+
+        number = float(input("Enter number: "))
+
+        print("Result =", square_root(number))
+
+        continue
+
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+
+    if choice == "1":
+        print("Result =", add(a, b))
+
+    elif choice == "2":
+        print("Result =", subtract(a, b))
+
+    elif choice == "3":
+        print("Result =", multiply(a, b))
+
+    elif choice == "4":
+        print("Result =", divide(a, b))
+
+    elif choice == "5":
+        print("Result =", power(a, b))
+
+    elif choice == "7":
+        print("Result =", modulus(a, b))
+
+    elif choice == "8":
+        print("Result =", floor_division(a, b))
+
+    else:
+        print("Invalid choice!")
